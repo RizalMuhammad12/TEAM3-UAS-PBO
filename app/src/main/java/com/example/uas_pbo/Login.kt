1317fc1c4c0e3b91b890bc40e5add66b053d7270
RizalMuhammad12 <ryzengg123@gmail.com>
2026-06-21 21:18:14 +0800

    Splash + Login = Done

---
+++ app/src/main/java/com/example/uas_pbo/Login.kt
@@ -0,0 +1,51 @@
+package com.example.uas_pbo
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnLogin: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+        btnLogin = findViewById(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+
+            if(email == "root" &&
+                password == "root") {
+
+                Toast.makeText(
+                    this,
+                    "Login Berhasil",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                // Nanti arahkan ke HomeActivity
+                // startActivity(Intent(this, HomeActivity::class.java))
+
+            } else {
+
+                Toast.makeText(
+                    this,
+                    "Email atau Password Salah",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+}
